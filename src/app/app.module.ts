f1a733061d5883f13734a1c6e751123bc59e442d
Matt Stillson <mstillson@ultimatemedical.edu>
2017-04-20 16:30:33 -0400

    need to recommit all changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './start/app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StoreComponent } from './pages/store/store.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { DealsComponent } from './pages/deals/deals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StoreComponent,
+    BlogComponent,
+    DealsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/deals/deals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deals',
+  templateUrl: './deals.component.html',
+  styleUrls: ['./deals.component.scss']
+})
+export class DealsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
